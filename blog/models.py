82132777a6a1e4a8c3c3db15dfdb217a28c88d97
jtaylor <jtaylor@cisco.com>
2024-02-16 14:41:09 +0000

    Created basic models for author and blog in models. Applied migration

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Author(models.Model):
+    name = models.CharField(max_length=200)
+    bio = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+class BlogPost(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    pub_date = models.DateTimeField('date published')
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
