332f26e411451ea7e973373e145e02d2258b27d8
jtaylor <jtaylor@cisco.com>
2024-02-16 14:55:50 +0000

    Create blog index view, use index as default page for blog. Create blog post in database

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import BlogPost
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello World. You're at the blog index.")
+    posts = BlogPost.objects.all().order_by('-pub_date')
+    return render(request, 'blog_index.html', {'posts': posts})
+    # return HttpResponse("Hello World. You're at the blog index.")
